0d1df3de449f952c048405259352e02e0de4174f
Aepu <muhammadagilfadilah@gmail.com>
2025-04-17 23:27:32 +0800

    added top down move

--- scenes/player/topdown_movement.gd
+++ scenes/player/topdown_movement.gd
@@ -1,11 +1,18 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var speed: float = 200.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var input_dir: Vector2
+
+@onready var body: CharacterBody2D = get_parent()
+
+
+func _input(event: InputEvent) -> void:
+	input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+
+func _physics_process(delta: float) -> void:
+	body.velocity = input_dir * speed
+	body.move_and_slide()
