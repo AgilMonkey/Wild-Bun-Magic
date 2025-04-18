b888b1f4deb16f29154caf0c02e1e64c5597d718
Aepu <muhammadagilfadilah@gmail.com>
2025-04-18 14:52:51 +0800

    Added reload ui

--- entities/ui/spell_ui.gd
+++ entities/ui/spell_ui.gd
@@ -5,3 +5,9 @@ func _process(delta: float) -> void:
 	if GlobalUi.cur_spell == null: return
 	$VBoxContainer/SpellLabel.text = "Spell: " + GlobalUi.cur_spell.spell_name
 	$VBoxContainer/AmmoLabel.text = "Ammo: " + str(GlobalUi.cur_spell.ammo)
+	
+	var can_shoot = not GlobalUi.cur_spell.is_shooting
+	if can_shoot:
+		$VBoxContainer/ReloadLabel.text = "Can Shoot!"
+	else:
+		$VBoxContainer/ReloadLabel.text = "Reloading..."
