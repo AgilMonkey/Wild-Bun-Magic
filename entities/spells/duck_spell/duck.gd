600bcaaf66b2ee001c0f1228617dba9f03d1cb4a
Aepu <muhammadagilfadilah@gmail.com>
2025-04-20 02:27:52 +0800

    Make sure duck is gone soon

--- entities/spells/duck_spell/duck.gd
+++ entities/spells/duck_spell/duck.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
 
+func _ready() -> void:
+	await get_tree().create_timer(10.0).timeout
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
