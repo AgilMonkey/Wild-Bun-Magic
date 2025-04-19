3705c411f27fb3b8c9d2a2981373c07bd9911ce0
Aepu <muhammadagilfadilah@gmail.com>
2025-04-20 03:12:00 +0800

    Make pickupable able to be pick when dashing

--- entities/health_drop/health_drop.gd
+++ entities/health_drop/health_drop.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 
+@export var health_up_audio: AudioStream 
 @export var health_increase: int = 25
 @export var pickup_timer: float = 10.0
 
@@ -12,6 +13,7 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if body.has_method("add_player_health"):
-			body.add_player_health(health_increase)
+		if body.get_parent().has_method("add_player_health"):
+			body.get_parent().add_player_health(health_increase)
+			SoundManager.play_sound(health_up_audio)
 			queue_free()
