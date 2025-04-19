ab532fa45c4c7d900fcae2ba89ca3618314b8ab2
Aepu <muhammadagilfadilah@gmail.com>
2025-04-19 17:53:19 +0800

    Make health pickup gone in 10s

--- entities/health_drop/health_drop.gd
+++ entities/health_drop/health_drop.gd
@@ -2,6 +2,12 @@ extends Area2D
 
 
 @export var health_increase: int = 25
+@export var pickup_timer: float = 10.0
+
+
+func _ready() -> void:
+	await get_tree().create_timer(pickup_timer).timeout
+	queue_free()
 
 
 func _on_body_entered(body: Node2D) -> void:
