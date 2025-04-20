c948f269a6e31372d21915f81277b2b029221ea9
Aepu <muhammadagilfadilah@gmail.com>
2025-04-21 01:10:28 +0800

    Dissable important stuff when enemy death

--- entities/enemies/simple_enemy/enemy.gd
+++ entities/enemies/simple_enemy/enemy.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 
 @export var health_pickup_instance: PackedScene
 @export var chance_to_drop_health = 0.25
+@export var nodes_to_dissable: Array[Node]
 
 @onready var enemy_anim_player: AnimationPlayer = $EnemyAnimPlayer
 @onready var follow_player: Node = $FollowPlayer
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
 func _on_health_component_health_reached_zero() -> void:
@@ -13,11 +15,14 @@ func _on_health_component_health_reached_zero() -> void:
 	Global.player_killed_an_enemy.emit()
 	
 	follow_player.process_mode = Node.PROCESS_MODE_DISABLED
+	collision_shape_2d.call_deferred("set_disabled", true)
+	for node: Node in nodes_to_dissable:
+		node.process_mode = Node.PROCESS_MODE_DISABLED
 	enemy_anim_player.play("enemy_anim_lib/death")
 
 
 func drop_pickups():
-	if randf_range(0.0, 1.0) < chance_to_drop_health:
+	if randf_range(0.00, 1.00) < chance_to_drop_health:
 		drop_health()
 
 
