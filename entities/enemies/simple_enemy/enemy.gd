e6930ff9af0dbff04d2918469f8917d8cbeaf791
Aepu <muhammadagilfadilah@gmail.com>
2025-04-21 01:21:52 +0800

    Added dead enemy sound

--- entities/enemies/simple_enemy/enemy.gd
+++ entities/enemies/simple_enemy/enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 
+const ENEMY_HIT_SOUND = preload("res://assets/enemy_hit.wav")
+
 @export var health_pickup_instance: PackedScene
 @export var chance_to_drop_health = 0.25
 @export var nodes_to_dissable: Array[Node]
@@ -8,7 +10,7 @@ extends CharacterBody2D
 @onready var enemy_anim_player: AnimationPlayer = $EnemyAnimPlayer
 @onready var follow_player: Node = $FollowPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 func _on_health_component_health_reached_zero() -> void:
 	drop_pickups()
@@ -16,8 +18,10 @@ func _on_health_component_health_reached_zero() -> void:
 	
 	follow_player.process_mode = Node.PROCESS_MODE_DISABLED
 	collision_shape_2d.call_deferred("set_disabled", true)
+	hitbox_component.call_deferred("queue_free")
 	for node: Node in nodes_to_dissable:
 		node.process_mode = Node.PROCESS_MODE_DISABLED
+	SoundManager.play_sound(ENEMY_HIT_SOUND)
 	enemy_anim_player.play("enemy_anim_lib/death")
 
 
