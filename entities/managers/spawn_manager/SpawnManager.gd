209f1555f1f37d24e915ff47235d2519dca6be96
Aepu <muhammadagilfadilah@gmail.com>
2025-04-20 21:34:30 +0800

    Make enemy only spawn inside the wall

--- entities/managers/spawn_manager/SpawnManager.gd
+++ entities/managers/spawn_manager/SpawnManager.gd
@@ -2,6 +2,11 @@ class_name SpawnManager
 extends Node
 
 
+const WALL_UP: int = -2160
+const WALL_DOWN: int = 2160
+const WALL_LEFT: int = -3959
+const WALL_RIGHT: int = 3959
+
 @export var player: CharacterBody2D
 @export var enemy_to_spawn: Array[EnemyResource]
 
@@ -9,7 +14,7 @@ extends Node
 
 @export var spawn_count_rate := 30.0  # 1 spawn per second
 
-@export var spawn_min_range: int = 750  # From player
+@export var spawn_min_range: int = 1000  # From player
 @export var spawn_max_range: int = 1200
 @export var spawn_min_time: float = 0.9
 @export var spawn_max_time: float = 1.8
@@ -69,10 +74,22 @@ func get_random_enemy() -> PackedScene:
 
 func get_enemy_rand_position() -> Vector2:
 	var player_position = player.global_position
-	var rand_ang = randf_range(0, TAU)
-	var rand_range = randi_range(spawn_min_range, spawn_max_range)
-	var rand_vec = Vector2.RIGHT.rotated(rand_ang).normalized() * rand_range
+	var rand_vec = Vector2(5000, 5000)
+	while enemy_is_over_wall(rand_vec):
+		var rand_ang = randf_range(0, TAU)
+		var rand_range = randi_range(spawn_min_range, spawn_max_range)
+		rand_vec = Vector2.RIGHT.rotated(rand_ang).normalized() * rand_range
 	return player_position + rand_vec
+
+
+func enemy_is_over_wall(position: Vector2) -> bool:
+	var is_over_wall = (
+		position.x < WALL_LEFT or
+		position.x > WALL_RIGHT or
+		position.y < WALL_UP or
+		position.y > WALL_DOWN
+		)
+	return is_over_wall
 
 
 func _on_spawn_add_counter_timeout() -> void:
