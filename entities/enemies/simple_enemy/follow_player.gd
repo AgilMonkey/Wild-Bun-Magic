adae6d14382b1765a5fca985fe9d4209bd5d8833
Aepu <muhammadagilfadilah@gmail.com>
2025-04-21 00:10:03 +0800

    Add walk anim for enemy

--- entities/enemies/simple_enemy/follow_player.gd
+++ entities/enemies/simple_enemy/follow_player.gd
@@ -5,7 +5,11 @@ extends Node
 
 @onready var player: Node2D = get_tree().get_first_node_in_group("player")
 @onready var body: CharacterBody2D = get_parent()
+@onready var enemy_anim_player: AnimationPlayer = $"../EnemyAnimPlayer"
 
+
+func _ready() -> void:
+	enemy_anim_player.play("walk")
 
 func _physics_process(_delta: float) -> void:
 	var to_player = player.global_position - body.global_position
